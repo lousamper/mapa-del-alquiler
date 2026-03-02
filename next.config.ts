3815c26163c68a5ef3e084ac96511a74848feb60
Ludmila Samper <lou.samper@gmail.com>
2026-03-02 15:42:18 +0100

    blog

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx"],
 
-  // No sourcemaps (evita el crash del overlay)
   productionBrowserSourceMaps: false,
-
   experimental: {
     serverSourceMaps: false,
   },
 
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.devtool = false; // 🔥 corta los sourcemaps en dev
-    }
-    return config;
-  },
+  // para evitar el error de build con Turbopack en Next 16
+  turbopack: {},
 };
 
 export default nextConfig;
